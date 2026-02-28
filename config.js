e3b51a913c8d0724805aeef1714ef7f3c49abef0
rajkingdas23-eng <rajkingdas23@gmail.com>
2026-03-01 00:25:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1477374711087235082/BS9q12Z4dLCLEsD6oRbJWuqMXFmBwbD45HT0t0TWyDOGXgoNXuuKPyAX3SPf6AgcwfNT",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
